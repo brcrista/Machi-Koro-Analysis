312fed7ffed7e49844360d7d444c6d14504e5ce0
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-21 16:53:21 -0500

    Remodel the cards as classes

---
+++ src/machi_koro/__init__.py
@@ -0,0 +1,8 @@
+"""
+Defines types and functions for modeling Machi Koro.
+"""
+
+# __all__ = [
+#     "cards",
+#     "game"
+# ]
---
+++ src/machi_koro/cards.py
@@ -0,0 +1,204 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+class Color(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+    PURPLE = 3
+    GOLD = 4
+
+class Symbol(Enum):
+    WHEAT = 0
+    COW = 1
+    BOX = 2
+    CUP = 3
+    GEAR = 4
+    TOWER = 5
+    FACTORY = 6
+    FRUIT = 7
+
+class Card: ...
+
+@dataclass
+class Card:
+    color: Color
+    symbol: Symbol
+    cost: int
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        pass
+
+class WheatField(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.WHEAT
+        self.cost = 1
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 1
+
+class Ranch(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.COW
+        self.cost = 1
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 1
+
+class Bakery(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.BOX
+        self.cost = 1
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 1
+
+class Cafe(Card):
+    def __init__(self):
+        self.color = Color.RED
+        self.symbol = Symbol.CUP
+        self.cost = 2
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 1
+
+class ConvenienceStore(Card):
+    def __init__(self):
+        self.color = Color.GREEN
+        self.symbol = Symbol.BOX
+        self.cost = 2
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 3
+
+class Forest(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.GEAR
+        self.cost = 3
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 1
+
+class Stadium(Card):
+    def __init__(self):
+        self.color = Color.PURPLE
+        self.symbol = Symbol.TOWER
+        self.cost = 6
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 2 * (num_players - 1)
+
+class TvStation(Card):
+    def __init__(self):
+        self.color = Color.PURPLE
+        self.symbol = Symbol.TOWER
+        self.cost = 7
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 5
+
+class BusinessCenter(Card):
+    def __init__(self):
+        self.color = Color.PURPLE
+        self.symbol = Symbol.TOWER
+        self.cost = 8
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 0
+
+class CheeseFactory(Card):
+    def __init__(self):
+        self.color = Color.GREEN
+        self.symbol = Symbol.FACTORY
+        self.cost = 5
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        cow_cards = [x for x in hand if x.symbol == Symbol.COW]
+        return 3 * len(cow_cards)
+
+class FurnitureFactory(Card):
+    def __init__(self):
+        self.color = Color.GREEN
+        self.symbol = Symbol.FACTORY
+        self.cost = 3
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        gear_cards = [x for x in hand if x.symbol == Symbol.GEAR]
+        return 3 * len(gear_cards)
+class Mine(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.GEAR
+        self.cost = 6
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 5
+
+class FamilyRestaurant(Card):
+    def __init__(self):
+        self.color = Color.RED
+        self.symbol = Symbol.CUP
+        self.cost = 3
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 2
+
+class AppleOrchard(Card):
+    def __init__(self):
+        self.color = Color.BLUE
+        self.symbol = Symbol.WHEAT
+        self.cost = 3
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 3
+
+class FruitVegetableMarket(Card):
+    def __init__(self):
+        self.color = Color.GREEN
+        self.symbol = Symbol.FRUIT
+        self.cost = 2
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        wheat_cards = [x for x in hand if x.symbol == Symbol.WHEAT]
+        return 2 * len(wheat_cards)
+
+class TrainStation(Card):
+    def __init__(self):
+        self.color = Color.GOLD
+        self.symbol = Symbol.TOWER
+        self.cost = 4
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 0
+
+class ShoppingMall(Card):
+    def __init__(self):
+        self.color = Color.GOLD
+        self.symbol = Symbol.TOWER
+        self.cost = 10
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 0
+
+class AmusementPark(Card):
+    def __init__(self):
+        self.color = Color.GOLD
+        self.symbol = Symbol.TOWER
+        self.cost = 16
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 0
+
+class RadioTower(Card):
+    def __init__(self):
+        self.color = Color.GOLD
+        self.symbol = Symbol.TOWER
+        self.cost = 22
+
+    def revenue(self, hand: List[Card], num_players: int) -> int:
+        return 0
